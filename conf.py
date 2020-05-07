37b4bb3da9a5faca35a05cbb612055917c7502d1
Andrea Diamantini <adjam@protonmail.com>
2020-05-07 17:09:22 +0200

    CSS update and... myrules added (actually youtube and hr)

--- conf.py
+++ conf.py
@@ -2,6 +2,11 @@
 #
 # Documentation: https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# my rules for rest addons
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+import myrules
 
 # -- Project information -----------------------------------------------------
 
